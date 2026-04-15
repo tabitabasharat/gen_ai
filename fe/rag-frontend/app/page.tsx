3555930712a5cb8f6bd1da9cf151d5823cfe6fd1
tabitabasharat <tabitab632@gmail.com>
2026-04-15 11:14:43 +0500

    --Build----

--- fe/rag-frontend/app/page.tsx
+++ fe/rag-frontend/app/page.tsx
@@ -3,7 +3,7 @@ import ChatWindow from "../components/ChatWindow";
 export default function Home() {
   return (
     <main className="flex flex-col gap-3 items-center justify-center h-screen bg-gray-100">
-        <h1 className="text-black font-bold">ChatBot AI (RAG)</h1>
+        <h1 className="text-black font-bold">ChatBot AI </h1>
 
       <ChatWindow />
     </main>
